9874ddf0be2973abfb8e5824e8669000d5899498
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-03-31 18:23:17 +0200

    rvtl.js update & bot sends messages now eggzdee

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 import { Client } from "revolt.js";
-const client = new Client;
+let client = new Client();
 
 import fs from 'fs';
 
@@ -9,10 +9,10 @@ const prefix = '!';
 /*
 To note: This bot is coded in SYSlang originally, meaning I had no JS to work with; Coded from scratch by an idiot.
 
-Parsed into Revolt by TallerThanShort with permission from V1RU5 and other members of the og bot dev team.
+Parsed into Discord.js then Revolt.js by TallerThanShort with permission from V1RU5 and other members of the og bot dev team.
 */
 const botVersion = 'v7.4.2 (Alpha)'
-const upDate = '12/03/2022 at 17:48 pm'
+const upDate = '31/03/2022 at 18:20 pm'
 
 
 /* This is apparently how we set bot statuses, but didn't work for me
@@ -21,43 +21,29 @@ client.req("PATCH", `/users/@me`, {
 });*/
 
 client.on("ready", async () => {
-    console.log('SYS4Revolt is ready!');
+    console.log(`Logged in as ${client.user.username}!`);
 });
 
 // I have absolutely no clue how to make commands 'n shit
-client.on('message', async (message) =>{
+client.on("message", async (message) =>{
     if (typeof message.content !== "string") return;
-    if (!message.server) return;
+    // this works in reverse? if (message.server) return;
     if(!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
+    console.log(message.content);
 
-    if(message.content === '!ping'){
-        console.log('message.content');
-        message.channel.sendMessage("Pong!");
-    } /*else if(command === 'help'){
-        Client.commands.get('help').execute(message, args, Discord);
+    if(command === 'ping'){
+        message.channel?.sendMessage("Pong!");
+    } else if(command === 'help'){
+        message.channel?.sendMessage("WIP");
     } else if(command === 'info'){
-        Client.commands.get('info').execute(message, args, Discord, Client, botVersion, upDate);
+        message.channel?.sendMessage("Bot Version " + botVersion + `\n Last updated: ` + upDate);
     } else if(command === 'p'){
-        message.channel.send('parcel');
-    } else if(command === 'lol'){
-        Client.commands.get('lols').execute(message, args);
-    } else if(command === 'clear'){
-        Client.commands.get('clear').execute(message, args);
-    } else if(command === 'ched'){
-        Client.commands.get('ched').execute(message, args, Discord);
-    } else if(command === 'warn'){
-        Client.commands.get('warn').execute(message, args);
+        message.channel?.sendMessage('parcel');
     } else if(command === 'avatar'){
-        message.channel.send(message.author.avatarURL())
-    } else if(command === 'explain'){
-        Client.commands.get('explain').execute(message, args, Client);
-    } else if(command === 'ban'){
-        Client.commands.get('ban').execute(message, args);
-    } else if(command === 'kick'){
-        Client.commands.get('kick').execute(message, args);
-    }*/
+        message.channel?.sendMessage(message.author.username)
+    }
 });
 
 client.loginBot(process.env.token);
