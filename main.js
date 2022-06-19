57f6a14fb84701bcab00e2e59f1313611bd7eaa0
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-06-19 18:18:15 +0200

    Update v7.9.4

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ To note: This bot is coded in SYSlang originally, meaning I had no JS to work wi
 
 Parsed into Discord.js then Revolt.js by TallerThanShort with permission from V1RU5 and other members of the og bot dev team.
 */
-const botVersion = 'v7.9 (Beta)'
-const upDate = '18/06/2022 at 21:27 pm CEST'
+const botVersion = 'v7.9.4 (Beta)'
+const upDate = '19/06/2022 at 18:24 pm CEST'
 
 
-/* This is apparently how we set bot statuses, but didn't work for me
+/* This is how we set bot statuses, but it requires importing the main API
 client.req("PATCH", `/users/@me`, {
 	status: { presence: "Busy" }, //  you can also use "Idle", "Busy" or "Invisible" (the latter will make your bot appear offline though)
 });*/
@@ -45,12 +45,13 @@ client.on("message", async (message) =>{
     } else if(command === 'help'){
         message.channel?.sendMessage(`Commands: \nPing \nHelp \nInfo \np \navatar`);
     } else if(command === 'info'){
-        message.channel?.sendMessage("Bot Version " + botVersion + `\n Last updated: ` + upDate);
+        message.channel?.sendMessage("Bot Version " + botVersion + `\n Last updated: ` + upDate + `\n  This bot has a [localhost:9000 website](https://ckstudios2018.github.io/syslbot/#revolt)`);
     } else if(command === 'p'){
         message.channel?.sendMessage('parcel');
     } else if(command === 'avatar'){
-        message.channel?.sendMessage(`${message.author.username}\n` + 'https://autumn.revolt.chat/' + message.author.avatar.tag + '/' + message.author.avatar._id + '?max_side=256')
-    }
+        message.channel?.sendMessage(`${message.author.username}\n` + 'https://autumn.revolt.chat/' + message.author.avatar.tag + '/' + message.author.avatar._id + '?max_side=256');
+    } else if(command === 'invite'){
+        message.channel?sendMessage(`[invite](https://app.revolt.chat)`);
 });
 
 client.loginBot(process.env.token);
